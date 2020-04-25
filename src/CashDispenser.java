5ce085117e0c9d0c517f0413e881082964ad2f1f
mughees-asif <mughees460@gmail.com>
2020-04-25 17:07:37 +0100

    CashDispenser Class

--- src/CashDispenser.java
+++ src/CashDispenser.java
@@ -1,3 +1,33 @@
-
+// represents the cash dispenser of the ATM
 public class CashDispenser {
+
+    // default number of bills in the cash dispenser
+    private final static int INITIAL_COUNT = 500;
+    // number of bill remaining
+    private int count;
+
+    // no-argument constructor initialises count to default
+    public CashDispenser() {
+        count = INITIAL_COUNT;
+    }
+
+    // simulates dispensing of specified amount of cash
+    public void dispenseCash (int amount) {
+        // number of £20 bills required
+        int billsRequired = amount / 20;
+        // update count of bills
+        count -= billsRequired;
+    }
+    
+    // indicates whether the cash dispenser can dispense desired amount
+    public boolean isSufficientCashAvailable(int amount) {
+        // number of £20 bills required
+        int billsRequired = amount / 20;
+        // check to see if enough bills available
+        if (count >= billsRequired) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
